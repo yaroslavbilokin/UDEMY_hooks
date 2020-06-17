5dc8bb2f6e470d03849798b617c8adb3501e56bc
yaroslavbilokin <yaroslavbilokin41@gmail.com>
2020-06-18 00:15:18 +0300

    initial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const App = () => {
+    const [value, setValue] = useState(0);
+    const [visible, setVisible] = useState(true);
+
+    if (visible) {
+        return (
+            <div>
+                <button onClick={() => setValue((v) => v + 1)}>+</button>
+                <button onClick={() => setVisible(false)}>hide</button>
+                <ClassCounter value={value} />
+                <HookCounter value={value} />
+            </div>
+        );
+    } else {
+        return <button onClick={() => setVisible(true)}>show</button>
+    }
+}
+
+const HookCounter = ({ value }) => {
+    useEffect(() => {
+        console.log('useEffect()');
+
+        return () => console.log('clear')
+    }, [value])
+    return <p>{value}</p>
+}
+
+class ClassCounter extends React.Component {
+    componentDidMount() {
+        console.log('classs: mount')
+    }
+
+    componentDidUpdate(props) {
+        console.log('c: update')
+    }
+
+    componentWillUnmount() {
+        console.log('c: unmount')
+    }
+
+    render() {
+        return <p>{this.props.value}</p>
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,
+    document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
